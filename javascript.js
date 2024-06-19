a405bf0b0c95b609b93b552f11f6c1539f122245
DaniloMoncaodeAraujo <danilomoncaodearaujo@gmail.com>
2024-06-19 11:07:47 -0300

    Primeiro Commit

---
+++ javascript.js
@@ -0,0 +1,11 @@
+/*
+const menulist = document.getElementById("menu-list")
+menulist.style.maxHeight = "0px"
+
+function showMenu(){
+    if(menulist.style.maxHeight =="0px"){
+        menulist.style.maxHeight ="1000px"
+    } else{
+        menulist.style.maxHeight = "0px"
+    }
+}*/
